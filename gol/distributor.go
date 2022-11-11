34e78f99bedd51cb2dfc00b65bd1d37add497cb9
Oliver-Warren <aa20575@bristol.ac.uk>
2022-11-11 15:27:47 +0000

    encapsulated gol logic and tried to read in first world from io

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,6 +1,8 @@
 package gol
 
-import "GameOfLifeReal/util"
+import (
+	"GameOfLifeReal/util"
+)
 
 type distributorChannels struct {
 	events     chan<- Event
@@ -11,7 +13,25 @@ type distributorChannels struct {
 	ioInput    <-chan uint8
 }
 
-func calculateNextState(p Params, world [][]byte) [][]byte {
+func makeByteArray(p Params) [][]byte {
+	newArray := make([][]byte, p.ImageWidth)
+	for i := 0; i < p.ImageWidth; i++ {
+		newArray[i] = make([]byte, p.ImageHeight)
+	}
+	return newArray
+}
+
+func loadFirstWorld(p Params, firstWorld [][]byte, c distributorChannels) {
+	c.ioCommand <- 1
+	c.ioFilename <- "16x16"
+	for i := 0; i < p.ImageWidth; i++ {
+		for j := 0; j < p.ImageHeight; j++ {
+			firstWorld[i][j] = <-c.ioInput
+		}
+	}
+}
+
+func calculateNextState(p Params, world [][]byte, c distributorChannels, turn int) [][]byte {
 	sum := 0
 	newWorld := make([][]byte, p.ImageWidth)
 	for i := 0; i < p.ImageWidth; i++ {
@@ -30,14 +50,17 @@ func calculateNextState(p Params, world [][]byte) [][]byte {
 			if world[y][x] == 255 {
 				if sum < 2 {
 					newWorld[y][x] = 0
+					c.events <- CellFlipped{turn, util.Cell{x, y}}
 				} else if sum == 2 || sum == 3 {
 					newWorld[y][x] = 255
 				} else {
-					newWorld[x][y] = 0
+					newWorld[y][x] = 0
+					c.events <- CellFlipped{turn, util.Cell{x, y}}
 				}
 			} else {
 				if sum == 3 {
 					newWorld[y][x] = 255
+					c.events <- CellFlipped{turn, util.Cell{x, y}}
 				} else {
 					newWorld[y][x] = 0
 				}
@@ -47,15 +70,15 @@ func calculateNextState(p Params, world [][]byte) [][]byte {
 	return newWorld
 }
 
-func gameOfLife(p Params, initialWorld [][]byte) [][]byte {
-	world := initialWorld
+func gameOfLife(p Params, world [][]byte, c distributorChannels) [][]byte {
 	for turn := 0; turn < p.Turns; turn++ {
-		world = calculateNextState(p, world)
+		world = calculateNextState(p, world, c, turn)
+		c.events <- TurnComplete{turn}
 	}
 	return world
 }
 
-func calculateAliveCells(p Params, world [][]byte) []util.Cell {
+func calculateAliveCells(p Params, world [][]byte, c distributorChannels) []util.Cell {
 	aliveCells := make([]util.Cell, 0)
 	for x := 0; x < p.ImageWidth; x++ {
 		for y := 0; y < p.ImageHeight; y++ {
@@ -71,23 +94,14 @@ func calculateAliveCells(p Params, world [][]byte) []util.Cell {
 func distributor(p Params, c distributorChannels) {
 
 	// TODO: Create a 2D slice to store the world.
-	newWorld := make([][]byte, p.ImageWidth)
-	for i := 0; i < p.ImageWidth; i++ {
-		newWorld[i] = make([]byte, p.ImageHeight)
-	}
-	for i := 0; i < p.ImageWidth; i++ {
-		for j := 0; j < p.ImageHeight; j++ {
-			newWorld[i][j] = <-c.ioInput
-		}
-
-	}
-
+	firstWorld := makeByteArray(p)
+	// Get initial world as input from io channel and populate
+	loadFirstWorld(p, firstWorld, c)
 	// TODO: Execute all turns of the Game of Life.
-	for turn := 0; turn < p.Turns; turn++ {
-		newWorld = calculateNextState(p, newWorld)
-	}
+	finalWorld := makeByteArray(p)
+	finalWorld = gameOfLife(p, firstWorld, c)
 	// TODO: Report the final state using FinalTurnCompleteEvent.
-	c.events <- FinalTurnComplete{p.Turns, calculateAliveCells(p, newWorld)}
+	c.events <- FinalTurnComplete{p.Turns, calculateAliveCells(p, finalWorld, c)}
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle

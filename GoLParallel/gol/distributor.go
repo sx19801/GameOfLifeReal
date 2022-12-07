e24531423da3c106e48f12800ea8b83f34cd416c
sx19801 <sx19801@bristol.ac.uk>
2022-12-07 15:17:46 +0000

    tidying things up

--- GoLParallel/gol/distributor.go
+++ GoLParallel/gol/distributor.go
@@ -17,11 +17,11 @@ type distributorChannels struct {
 	ioInput    <-chan uint8
 }
 
-type worldSegment struct {
-	segment [][]uint8
-	start   int
-	length  int
-}
+// type worldSegment struct {
+// 	segment [][]uint8
+// 	start   int
+// 	length  int
+// }
 
 var turn int
 var pausing bool
@@ -56,76 +56,76 @@ func outputWorld(p Params, world [][]byte, c distributorChannels, turn int) {
 	}
 }
 
-func makeWorldSegment(p Params, i int, n int, withFringes bool) worldSegment {
-	segLength := getSegLength(p, i, n)
-	if withFringes {
-		segLength = segLength + 2
-	}
-	return worldSegment{
-		segment: makeByteArray(p.ImageWidth, segLength),
-		start:   getSegStart(p, i, n),
-		length:  segLength,
-	}
-}
+// func makeWorldSegment(p Params, i int, n int, withFringes bool) worldSegment {
+// 	segLength := getSegLength(p, i, n)
+// 	if withFringes {
+// 		segLength = segLength + 2
+// 	}
+// 	return worldSegment{
+// 		segment: makeByteArray(p.ImageWidth, segLength),
+// 		start:   getSegStart(p, i, n),
+// 		length:  segLength,
+// 	}
+// }
 
 // i is segment number and n is number of threads
-func getSegStart(p Params, i int, n int) int {
-	if p.ImageHeight%n == 0 {
-		return (p.ImageHeight / n) * i
-	} else {
-		if i != n-1 {
-			return ((p.ImageHeight-(p.ImageHeight%n-1))/n - 1) * i
-		} else {
-			return p.ImageHeight - (p.ImageHeight % (n - 1))
-		}
-	}
-}
+// func getSegStart(p Params, i int, n int) int {
+// 	if p.ImageHeight%n == 0 {
+// 		return (p.ImageHeight / n) * i
+// 	} else {
+// 		if i != n-1 {
+// 			return ((p.ImageHeight-(p.ImageHeight%n-1))/n - 1) * i
+// 		} else {
+// 			return p.ImageHeight - (p.ImageHeight % (n - 1))
+// 		}
+// 	}
+// }
 
-func getSegLength(p Params, i int, n int) int {
-	if p.ImageHeight%n == 0 {
-		return p.ImageHeight / n
-	} else {
-		if i != n-1 {
-			return (p.ImageHeight-(p.ImageHeight%n-1))/n - 1
-		} else {
-			return p.ImageHeight%n - 1
-		}
-	}
-}
+// func getSegLength(p Params, i int, n int) int {
+// 	if p.ImageHeight%n == 0 {
+// 		return p.ImageHeight / n
+// 	} else {
+// 		if i != n-1 {
+// 			return (p.ImageHeight-(p.ImageHeight%n-1))/n - 1
+// 		} else {
+// 			return p.ImageHeight%n - 1
+// 		}
+// 	}
+// }
 
 // col is y coordinate and row is x
-func splitWorld(p Params, i int, n int, firstWorld [][]uint8) worldSegment {
-	// work out how big the segment needs to be, allocate memory
-	seg := makeWorldSegment(p, i, n, true)
-	if i != 0 && i != n-1 { // if not the first or last segment
-		for row := 0; row < seg.length; row++ {
-			for col := 0; col < p.ImageWidth; col++ {
-				seg.segment[row][col] = firstWorld[seg.start+row-1][col]
-			}
-		}
-	} else if i == 0 { // if first segment
-		for col := 0; col < p.ImageWidth; col++ {
-			// copy bottom of world in (fringes)
-			seg.segment[0][col] = firstWorld[p.ImageHeight-1][col]
-		}
-		for row := 0; row < seg.length; row++ {
-			for col := 0; col < p.ImageWidth; col++ {
-				seg.segment[row][col] = firstWorld[seg.start+row][col] // no -1 bcos 1st seg
-			}
-		}
-	} else if i == n-1 { // if last segment
-		for col := 0; col < p.ImageWidth; col++ {
-			// copy top of world into bottom row of segment (fringes)
-			seg.segment[seg.length-1][col] = firstWorld[0][col]
-		}
-		for row := 0; row < seg.length-1; row++ {
-			for col := 0; col < p.ImageWidth; col++ {
-				seg.segment[row][col] = firstWorld[seg.start+row-1][col]
-			}
-		}
-	}
-	return seg
-}
+// func splitWorld(p Params, i int, n int, firstWorld [][]uint8) worldSegment {
+// 	// work out how big the segment needs to be, allocate memory
+// 	seg := makeWorldSegment(p, i, n, true)
+// 	if i != 0 && i != n-1 { // if not the first or last segment
+// 		for row := 0; row < seg.length; row++ {
+// 			for col := 0; col < p.ImageWidth; col++ {
+// 				seg.segment[row][col] = firstWorld[seg.start+row-1][col]
+// 			}
+// 		}
+// 	} else if i == 0 { // if first segment
+// 		for col := 0; col < p.ImageWidth; col++ {
+// 			// copy bottom of world in (fringes)
+// 			seg.segment[0][col] = firstWorld[p.ImageHeight-1][col]
+// 		}
+// 		for row := 0; row < seg.length; row++ {
+// 			for col := 0; col < p.ImageWidth; col++ {
+// 				seg.segment[row][col] = firstWorld[seg.start+row][col] // no -1 bcos 1st seg
+// 			}
+// 		}
+// 	} else if i == n-1 { // if last segment
+// 		for col := 0; col < p.ImageWidth; col++ {
+// 			// copy top of world into bottom row of segment (fringes)
+// 			seg.segment[seg.length-1][col] = firstWorld[0][col]
+// 		}
+// 		for row := 0; row < seg.length-1; row++ {
+// 			for col := 0; col < p.ImageWidth; col++ {
+// 				seg.segment[row][col] = firstWorld[seg.start+row-1][col]
+// 			}
+// 		}
+// 	}
+// 	return seg
+// }
 
 // GOL LOGIC. Has since been moved to worker
 
@@ -174,9 +174,6 @@ func keyPress(p Params, world [][]byte, c distributorChannels, turn int, keyPres
 func distributor(p Params, c distributorChannels, keyPresses <-chan rune) {
 	// PARALLEL GOL
 	// load initial world
-	//pausing = false
-	// pause := <-pausing
-	// fmt.Println(pause)
 	world := makeByteArray(p.ImageWidth, p.ImageHeight)
 	loadFirstWorld(p, world, c)
 
@@ -195,22 +192,23 @@ func distributor(p Params, c distributorChannels, keyPresses <-chan rune) {
 
 	ticker := time.NewTicker(2 * time.Second)
 	turn = 0
+	segmentHeight := p.ImageHeight / p.Threads
 
 	//select {}
 	// split world into segments, send each segment to each worker
 	for turn < p.Turns {
 		go twoSecondTicker(ticker, turn, p, world, c)
 		go keyPress(p, world, c, turn, keyPresses)
+
 		// BAD DIVISION
-		segmentHeight := p.ImageHeight / p.Threads
 
 		channels := make([]chan [][]byte, p.Threads)
+		var newWorld [][]byte
 		for i := range channels {
 			channels[i] = make(chan [][]byte)
 		}
 
 		if !pausing {
-
 			for i := 0; i < p.Threads; i++ {
 				if i == p.Threads-1 {
 					go calculateNextState(p, world, c, turn, segmentHeight*i, p.ImageHeight, channels[i])
@@ -219,15 +217,12 @@ func distributor(p Params, c distributorChannels, keyPresses <-chan rune) {
 				}
 			}
 
-			var newWorld [][]byte
 			for i := 0; i < p.Threads; i++ {
 				newWorld = append(newWorld, <-channels[i]...)
 			}
 
 			world = newWorld
-
 			turn++
-
 			c.events <- TurnComplete{turn}
 
 		}
--- GoLParallel/gol/worker.go
+++ GoLParallel/gol/worker.go
@@ -7,11 +7,12 @@ import (
 func calculateNextState(p Params, world [][]byte, c distributorChannels, turn int, start int, end int, channel chan [][]byte) { //worldSegment {
 	sum := 0
 	newWorld := make([][]byte, p.ImageWidth)
+	newSegment := make([][]byte, end-start)
+
 	for i := 0; i < p.ImageWidth; i++ {
 		newWorld[i] = make([]byte, p.ImageHeight)
 	}
 
-	newSegment := make([][]byte, end-start)
 	for i := 0; i < end-start; i++ {
 		newSegment[i] = make([]byte, p.ImageWidth)
 	}
@@ -52,56 +53,6 @@ func calculateNextState(p Params, world [][]byte, c distributorChannels, turn in
 	channel <- newSegment
 }
 
-// type workerChannels struct {
-// 	in  chan worldSegment
-// 	out chan worldSegment
-// }
-
-// // same as calculateNextState
-// func calculateNextStateOfSegmentWithFringes(p Params, world worldSegment, c distributorChannels, turn int) worldSegment {
-// 	sum := 0
-// 	// make smaller segment to return processed section without the fringes if more than one thread
-// 	newSegment := worldSegment{
-// 		segment: makeByteArray(p.ImageWidth, world.length-2),
-// 		start:   world.start,
-// 		length:  world.length - 2,
-// 	}
-// 	fmt.Println(world.start)
-
-// 	for y := 1; y < world.length-1; y++ {
-// 		for x := 0; x < p.ImageWidth; x++ {
-// 			sum = (int(world.segment[y-1][(x+p.ImageWidth-1)%p.ImageWidth]) +
-// 				int(world.segment[y-1][(x+p.ImageWidth)%p.ImageWidth]) +
-// 				int(world.segment[y-1][(x+p.ImageWidth+1)%p.ImageWidth]) +
-// 				int(world.segment[y][(x+p.ImageWidth-1)%p.ImageWidth]) +
-// 				int(world.segment[y][(x+p.ImageWidth+1)%p.ImageWidth]) +
-// 				int(world.segment[y+1][(x+p.ImageWidth-1)%p.ImageWidth]) +
-// 				int(world.segment[y+1][(x+p.ImageWidth)%p.ImageWidth]) +
-// 				int(world.segment[y+1][(x+p.ImageWidth+1)%p.ImageWidth])) / 255
-// 			if world.segment[y][x] == 255 {
-// 				if sum < 2 {
-// 					newSegment.segment[y-1][x] = 0
-// 					c.events <- CellFlipped{turn, util.Cell{x, world.start + y - 1}}
-// 				} else if sum == 2 || sum == 3 {
-// 					newSegment.segment[y-1][x] = 255
-// 					c.events <- CellFlipped{turn, util.Cell{x, world.start + y - 1}}
-// 				} else {
-// 					newSegment.segment[y-1][x] = 0
-// 					c.events <- CellFlipped{turn, util.Cell{x, world.start + y - 1}}
-// 				}
-// 			} else {
-// 				if sum == 3 {
-// 					newSegment.segment[y-1][x] = 255
-// 					c.events <- CellFlipped{turn, util.Cell{x, world.start + y - 1}}
-// 				} else {
-// 					newSegment.segment[y-1][x] = 0
-// 				}
-// 			}
-// 		}
-// 	}
-// 	return newSegment
-// }
-
 func calculateAliveCells(p Params, world [][]byte, c distributorChannels) []util.Cell {
 	aliveCells := make([]util.Cell, 0)
 	for x := 0; x < p.ImageWidth; x++ {
@@ -113,12 +64,3 @@ func calculateAliveCells(p Params, world [][]byte, c distributorChannels) []util
 	}
 	return aliveCells
 }
-
-// func work(w workerChannels, d distributorChannels, p Params, turn int) {
-// 	firstSegment := <-w.in
-// 	if p.Threads == 1 {
-// 		w.out <- calculateNextState(p, firstSegment.segment, d, turn)
-// 	} else {
-// 		w.out <- calculateNextStateOfSegmentWithFringes(p, firstSegment, d, turn)
-// 	}
-// }
